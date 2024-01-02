a54e2ccfb6b6a0d8909701a121ed4f662f741746
HelloMyNameIsSeth <seth.touchette@gmail.com>
2024-01-02 14:28:49 +1030

    oncollection change send message to discord channel

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import pymongo
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -7,6 +8,10 @@ intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+mongo_client = pymongo.MongoClient(os.getenv('CLIENT_STRING'))
+database = mongo_client['LongLostStakingDatabase']
+collection = database['orders']
 
 @bot.event
 async def on_ready():
@@ -16,5 +21,17 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('pong')
 
+@collection.watch()
+async def on_collection_change(change):
+    if change['operationType'] == 'insert':
+        new_entry = change['fullDocument']
+        channel_id = '779388088706662463'  # Replace with your channel ID
+        channel = bot.get_channel(channel_id)
+
+        if channel:
+            await channel.send(f'New entry added: {new_entry}')
+
 if __name__ == '__main__':
     bot.run(os.getenv('DISCORD_TOKEN'))
+
+
